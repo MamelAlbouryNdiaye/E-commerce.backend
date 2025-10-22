abe3aa026877c201232808343fb4d0b6456edfda
Mamel Alboury NDIAYE <Mamel Aly>
2025-10-22 20:36:39 +0200

    ESM : create_dirname

--- server.mjs
+++ server.mjs
@@ -1,12 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import connectDB from './config/db.mjs';
+import { fileURLToPath } from 'url';
+import path from 'path';
 
 ////// setup  //////
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3030;
 
+////////// ESM: create __dirname ///////////////
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // db connection
 connectDB();
